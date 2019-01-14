c914ad69da1ac1b533c1514c1f000f3b2cbba6f4
Nalipp <nateplipp@gmail.com>
2019-01-14 15:16:40 -0800

    update cities in database every 2hours

---
+++ api/helpers/api_calls.js
@@ -0,0 +1,88 @@
+var axios = require('axios');
+var db = require('../models');
+var allCities = require('./cities_list').allCities;
+var DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
+
+function initializeCityData() {
+  // db.forcasts.drop() before running so there are no duplicates
+  var promises = [];
+
+  allCities.forEach(function(city) {
+    var curCityName = city[0];
+    var lng = city[1];
+    var lat = city[2];
+    var promise = axios.get(`https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${lng},${lat}?exclude=minutely,hourly`, { cityName: curCityName})
+    promises.push(promise);
+  });
+
+  Promise.all(promises).then(function(results) {
+    results.forEach(function(city) {
+      var currentCity = {
+        cityName: city.config.cityName,
+        summary: city.data.currently.summary,
+        icon: city.data.currently.icon,
+        precipIntensity: city.data.currently.precipIntensity,
+        precipProbability: city.data.currently.precipProbability,
+        temperature: city.data.currently.temperature,
+        apparentTemperature: city.data.currently.apparentTemperature,
+        humidity: city.data.currently.humidity,
+        windSpeed: city.data.currently.windSpeed,
+        cloudCover: city.data.currently.cloudCover,
+        uvIndex: city.data.currently.uvIndex,
+        visibility: city.data.currently.visibility
+      };
+
+      db.Forcast.create(currentCity)
+      .then(function(res) {
+        console.log('successful initialization...', currentCity.cityName);
+      })
+      .catch(function(err) {
+        console.log('err initializing database...', err);
+      })
+    })
+  })
+}
+
+function updateCityData() {
+  var promises = [];
+
+  allCities.forEach(function(city) {
+    var curCityName = city[0]
+    var lng = city[1];
+    var lat = city[2];
+    var promise = axios.get(`https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${lng},${lat}?exclude=minutely,hourly`, { cityName: curCityName})
+    promises.push(promise);
+  });
+
+  Promise.all(promises).then(function(results) {
+    results.forEach(function(city) {
+      db.Forcast.update({cityName: city.config.cityName}, {
+          cityName: city.config.cityName,
+          summary: city.data.currently.summary,
+          temperature: city.data.currently.summary,
+          icon: city.data.currently.icon,
+          precipIntensity: city.data.currently.precipIntensity,
+          precipProbability: city.data.currently.precipProbability,
+          temperature: city.data.currently.temperature,
+          apparentTemperature: city.data.currently.apparentTemperature,
+          humidity: city.data.currently.humidity,
+          windSpeed: city.data.currently.windSpeed,
+          cloudCover: city.data.currently.cloudCover,
+          uvIndex: city.data.currently.uvIndex,
+          visibility: city.data.currently.visibility
+        }).then(function(res) {
+          console.log('successful update...', city.config.cityName);
+        })
+        .catch(function(err) {
+          console.log('err updating database...', err);
+        });
+    })
+  })
+}
+
+module.exports = {
+  initializeCityData: initializeCityData,
+  updateCityData: updateCityData,
+}
+
+
--- api/index.js
+++ api/index.js
@@ -5,7 +5,8 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var db = require('./models');
 var axios = require('axios');
-    
+var updateCityData = require('./helpers/api_calls').updateCityData;    
+
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,6 +20,10 @@ app.get('/api/forcasts/', function(req, res){
     res.send(err);
   })
 })
+
+setInterval(function() {
+  updateCityData(); // update cities in the database every 2 hours
+}, 7200000);
 
 app.listen(port, function(){
     console.log("APP IS RUNNING ON PORT " + port);
