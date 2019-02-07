32e00a1910a60212303e9a59df93e76b55b145ed
Nalipp <nateplipp@gmail.com>
2019-02-07 12:18:28 -0800

    MapImagesMap

--- client/src/components/MapImagesMap.js
+++ client/src/components/MapImagesMap.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
-import './MapImagesMap.css';
+const { compose, withProps, lifecycle } = require("recompose");
+const { withScriptjs, withGoogleMap, GoogleMap } = require("react-google-maps");
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+const googleUrl = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}&v=3.exp&libraries=geometry,drawing,places`
 
-class MapImagesMap extends Component {
-  render() {
-    return (
-      <div className={"map-images-map"}>
-        map
-      </div>
-    )
-  }
-}
+const MapImagesMap = compose(
+  withProps({
+    googleMapURL: googleUrl,
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `200px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  lifecycle({
+    componentWillMount() {
+      const refs = {}
+
+      this.setState({
+        onMapMounted: ref => {
+          refs.map = ref;
+        },
+        onBoundsChanged: () => {
+          console.log(refs.map.getBounds())
+        },
+      })
+    },
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    ref={props.onMapMounted}
+    zoom={15}
+    center={{ lat: 41.9, lng: -87.624 }}
+    options={{ disableDefaultUI: true, zoomControl: true, }}
+    onBoundsChanged={props.onBoundsChanged}
+  >
+  </GoogleMap>
+);
 
 export default MapImagesMap;
--- client/src/helpers/geoCoding.js
+++ client/src/helpers/geoCoding.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const getGeoCode = (city, cb) => {
   city = city.split(' ').join('+');
 
-  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyBvwtjPflzBTky_tl2BR8DeJBI_5HkK5zg`).then((res) => {
+  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${GOOGLE_API_KEY}`).then((res) => {
     const location = res.data.results[0].geometry.location;
     cb(location.lat, location.lng);
   })
