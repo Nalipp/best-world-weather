bae48d71103f99a563da8c3077a8850a2d068b18
Nalipp <nateplipp@gmail.com>
2019-02-11 11:16:35 -0800

    move display logic into App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,27 +5,48 @@ import ForcastList from './ForcastList';
 import Footer from './Footer';
 import SelectedCity from './SelectedCity';
 import Loader from './Loader';
+import { hideLocationDetail } from '../actions';
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
   render() {
+    const displayStatus = this.props.locationDetailOn ? 'display-on' : 'display-off';
     return (
       <div className={'app'}>
+
+        <div onClick={this.props.hideLocationDetail} className={`modal-background ${displayStatus}`}></div>
+
         <Loader />
+
         <Heading />
-          <div style={{display: 'flex'}}>
-            <div style={{flexGrow: '3'}}>
-              <ForcastFilter />
-              <ForcastList />
-            </div>
-            <div style={{flexGrow: '4'}}>
-              <SelectedCity />
-            </div>
+
+        <div style={{display: 'flex'}}>
+          <div className={`main-forcast-container`}>
+            <ForcastFilter />
+            <ForcastList />
           </div>
+          <div className={`main-cityDetail-container ${displayStatus}`}>
+            <SelectedCity />
+          </div>
+        </div>
+
         <Footer />
       </div>
     )
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    locationDetailOn: state.forcasts.locationDetailOn, 
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    hideLocationDetail: () => dispatch(hideLocationDetail()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- client/src/components/SelectedCity.js
+++ client/src/components/SelectedCity.js
@@ -4,8 +4,6 @@ import SelectedCityNavigation from './SelectedCityNavigation';
 import ForcastDetailContainer from './ForcastDetailContainer';
 import MapCompareContainer from './MapCompareContainer';
 import MapImagesContainer from './MapImagesContainer';
-import { connect } from 'react-redux';
-import { hideLocationDetail } from '../actions';
 import './SelectedCity.css';
 
 class SelectedCity extends Component {
@@ -19,36 +17,19 @@ class SelectedCity extends Component {
     this.setState({ selected: e.target.id })
   }
   render() {
-    const displayStatus = this.props.locationDetailOn ? 'display-on' : 'display-off';
     return (
       <div>
-        <div onClick={this.props.hideLocationDetail}
-          className={`modal-background ${displayStatus}`}></div>
-        <div className={`selected-city ${displayStatus}`}>
-          <Search searchType={'getForcast'} />
-          <SelectedCityNavigation 
-            handleSelect={this.handleSelect}
-            selected={this.state.selected} />
-          {this.state.selected === 'data' && <ForcastDetailContainer />}
-          {this.state.selected === 'maps' && <MapCompareContainer />}
-          {this.state.selected === 'images' && <MapImagesContainer />}
-        </div>
+        <Search searchType={'getForcast'} />
+        <SelectedCityNavigation 
+          handleSelect={this.handleSelect}
+          selected={this.state.selected} />
+        {this.state.selected === 'data' && <ForcastDetailContainer />}
+        {this.state.selected === 'maps' && <MapCompareContainer />}
+        {this.state.selected === 'images' && <MapImagesContainer />}
       </div>
     )
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    locationDetailOn: state.forcasts.locationDetailOn, 
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    hideLocationDetail: () => dispatch(hideLocationDetail()),
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SelectedCity);
+export default SelectedCity;
 
