f93f1cce06635d0fbb3ca603b9949e2c59c32774
Nalipp <nateplipp@gmail.com>
2019-02-15 10:42:29 -0800

    set cityDetail to the first filtered city

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,10 +4,15 @@ import Loader from './Loader';
 import Heading from './Heading';
 import ForcastContainer from './ForcastContainer';
 import SelectedCityContainer from './SelectedCityContainer';
+import { setLocationDetail } from '../actions';
 import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
+  componentDidUpdate() {
+    const { filteredForcasts, setLocationDetail } = this.props;
+    filteredForcasts.length > 0 && setLocationDetail(filteredForcasts[0]);
+  }
   render() {
     return (
       <div className={'app'}>
@@ -26,7 +31,14 @@ class App extends Component {
 const mapStateToProps = state => {
   return {
     locationDetailOn: state.forcasts.locationDetailOn, 
+    filteredForcasts: state.forcasts.filteredForcasts,
   }
 }
 
-export default connect(mapStateToProps, null)(App);
+const mapDispatchToProps = dispatch => {
+  return {
+    setLocationDetail: forcast => dispatch(setLocationDetail(forcast)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
