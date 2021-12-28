b4fe0d31cbdb2387369712c4104934468d138d68
nate lipp <natelipp@MacBook-Pro.local>
2021-12-27 18:51:13 -0800

    update code related breaking changes in google images api

--- client/src/helpers/getGeoCoords.js
+++ client/src/helpers/getGeoCoords.js
@@ -2,14 +2,16 @@ import axios from 'axios';
 const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 const getGeoCoords = (dispatch, count, bounds) => {
-  const lngs = bounds.ga;
-  const lats = bounds.ma;
-  const latsDiff = lats.l - lats.j;
-  const lngsDiff = lngs.l - lngs.j;
+  const lats = bounds.zb // references this format {g: h: } 
+  const lngs = bounds.Qa // references this format {g: h: } 
+
+  const latsDiff = lats.h - lats.g;
+  const lngsDiff = lngs.h - lngs.g;
 
   while (count > 0) {
-    const newLat = (Math.random() * latsDiff) + lats.j;
-    const newLng = (Math.random() * lngsDiff) + lngs.j;
+    const newLat = (Math.random() * latsDiff) + lats.g;
+    const newLng = (Math.random() * lngsDiff) + lngs.g;
+
 
     axios.get(`https://maps.googleapis.com/maps/api/streetview/metadata?size=600x300&location=${newLat},${newLng}&fov=90&heading=235&pitch=10&key=${GOOGLE_API_KEY}`).then(image => {
       if (image.data.status === 'OK') {
