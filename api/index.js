2f7fdc35199da4bcc303f44ce3ff038279894dc1
nate lipp <natelipp@MacBook-Pro.local>
2021-12-28 18:29:43 -0800

    add api/flight-test and api/flight/initizliae endpoints

--- api/index.js
+++ api/index.js
@@ -17,12 +17,6 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/initialize/', function(req, res){
-  // 'run db.forcasts.drop() from mongo prior to running'
-  getForcasts('initialize');
-  res.send('done');
-})
-
 app.get('/api/forcasts/', function(req, res){
   db.Forcast.find()
     .then(function(forcasts){
@@ -43,10 +37,47 @@ app.get('/api/populations/', function(req, res) {
     })
 });
 
+app.get('/api/forcast/initialize/', function(req, res){
+  // 'run db.forcasts.drop() from mongo prior to running'
+  getForcasts('initialize');
+  res.send('forcast/initialize...finished');
+});
+
+app.get('/api/flight/initialize/', function(req, res){
+  // allows you to get flight cost data for each city (writes to db)
+  // recursive setTimeout avoids rate limit
+  // takes about 20 minutes to run
+  function getNextForcast() {
+    if (worldCities.length === 0) {
+      console.log('*****flight requests complete exiting*****');
+      return;
+    } else {
+      setTimeout(function() {
+        const curCity = worldCities.pop();
+        console.log('processing...', curCity)
+        getFlights('SFO', curCity);
+        getNextForcast() 
+      }, 10000);
+    }
+  }
+  getNextForcast();
+
+  res.send('flight/initialize...finished');
+});
+
+app.get('/api/flight-test/', function(req, res) {
+  // endpoint for testing single flight price request (writes to db)
+  // worldCities[1] is [ 'Singapore', '1.290453', '103.852038', 'SIN' ]
+  getFlights('SFO', worldCities[1]);
+  res.send('/flight-test...finished');
+});
+
 setInterval(function() {
+  // updats the flight cost of a city once every hour
+  // the api rate limit of v2 is 2000 per month
   getFlights('sfo', worldCities[cityIdx % worldCities.length - 1]);
-  cityIdx += 1;
-}, 1200000);
+  cityIdx++;
+}, 3400000);
 
 setInterval(function() {
   getForcasts('update'); 
